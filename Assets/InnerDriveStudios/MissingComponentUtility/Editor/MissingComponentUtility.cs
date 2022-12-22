5410bf868c35ee9695c0cbb8546d559525ea3264
Inner Drive Studios <info@innerdrivestudios.com>
2022-12-22 21:54:44 +0100

    Added a missing component utility

---
+++ Assets/InnerDriveStudios/MissingComponentUtility/Editor/MissingComponentUtility.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace InnerDriveStudios.Util
+{
+	public static class MissingComponentUtility
+	{
+        private const string subMenu = "Missing Components/";
+
+		[MenuItem(Settings.menuPath+subMenu+"Select all non prefab game objects with missing components")]
+		private static void SelectAllNonPrefabGameObjectsWithMissingComponents()
+		{
+            //determine our root game objects to investigate
+            GameObject[] gameObjectsToTest =
+                    Common.GetEditMode() == Common.EditMode.NORMAL ?
+                    SceneManager.GetActiveScene().GetRootGameObjects():
+                    new GameObject[] { PrefabStageUtility.GetCurrentPrefabStage().prefabContentsRoot};
+
+            List<GameObject> goWithMissingComponents = new List<GameObject>();
+            foreach (GameObject go in gameObjectsToTest)
+			{
+                recursivelyFindAllGameObjectsWithMissingComponents(go, goWithMissingComponents);
+            }
+            
+            Selection.objects = goWithMissingComponents.ToArray();
+		}
+
+		private static void recursivelyFindAllGameObjectsWithMissingComponents(GameObject go, List<GameObject> goWithMissingComponents)
+		{
+            //should we add the current object to the list?
+            if (Common.IsNonPrefabGameObjectInstance(go) && GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(go) > 0)
+            {
+                goWithMissingComponents.Add(go);
+            }
+
+            //repeat for all children
+            foreach (Transform t in go.transform)
+			{
+                recursivelyFindAllGameObjectsWithMissingComponents(t.gameObject, goWithMissingComponents);
+			}
+		}
+
+        [MenuItem(Settings.menuPath+subMenu+"Select all prefab assets with missing components")]
+        public static void SelectAllPrefabAssetsWithMissingComponents()
+        {
+            // Get all prefab assets in the project
+            string[] guids = AssetDatabase.FindAssets("t:Prefab");
+
+            // Convert the guids to paths
+            string[] paths = new string[guids.Length];
+            for (int i = 0; i < guids.Length; i++)
+            {
+                paths[i] = AssetDatabase.GUIDToAssetPath(guids[i]);
+            }
+
+            List<UnityEngine.Object> objectsToSelect = new List<UnityEngine.Object>();
+
+            foreach (string path in paths)
+			{
+                GameObject go = (GameObject)AssetDatabase.LoadMainAssetAtPath(path);
+                if (go.GetComponentsInChildren<Component>().Where(x => x == null).Count() > 0)
+				{
+                    objectsToSelect.Add(go);
+				}
+            }
+       
+            Selection.objects = objectsToSelect.ToArray();
+        }
+
+        [MenuItem(Settings.menuPath + subMenu + "Remove missing components from all selected objects")]
+        private static void RemoveMissingComponentsFromAllSelectedObjects()
+        {
+            /*
+            GameObject[] gameObjectsToTest = Selection.gameObjects;
+            List<GameObject> goWithMissingComponents = new List<GameObject>();
+
+            foreach (GameObject go in gameObjectsToTest)
+            {
+                recursivelyFindAllGameObjectsWithMissingComponents(go, goWithMissingComponents);
+            }
+
+            foreach (GameObject go in goWithMissingComponents)
+            {
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+            }
+            */
+
+            foreach (GameObject go in Selection.gameObjects)
+            {
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+            }
+
+        }
+
+    }
+}
