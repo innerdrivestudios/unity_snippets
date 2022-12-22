ca0f3607ef93ff6eb415486b5e9fe8131d4c2f18
Inner Drive Studios <info@innerdrivestudios.com>
2022-12-22 13:25:58 +0100

    Added a pivot point utility

---
+++ Assets/InnerDriveStudios/PivotPointUtility/Editor/PivotUtility.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace InnerDriveStudios.Util
+{
+	public class PivotUtility
+	{
+
+		[MenuItem(Settings.menuPath + "Bottom Center Pivot")]
+		public static void BottomCenterPivot()
+		{
+			if (Common.IsInPlayMode())
+			{
+				EditorUtility.DisplayDialog("Error", Strings.ONLY_IN_EDITMODE, Strings.OK);
+				return;
+			}
+
+			if (Common.GetSelectedObjectsCount() != 1)
+			{
+				EditorUtility.DisplayDialog("Error", Strings.ONLY_ONE_ITEM, Strings.OK);
+				return;
+			}
+
+			if (!Common.IsRegularTransform(Selection.activeTransform))
+			{
+				EditorUtility.DisplayDialog("Error", Strings.NOT_REGULAR_TRANSFORM, Strings.OK);
+				return;
+			}
+
+			if (!Common.IsRootTransform(Selection.activeTransform))
+			{
+				EditorUtility.DisplayDialog("Error", Strings.NOT_ROOT_TRANSFORM, Strings.OK);
+				return;
+			}
+
+			if (Common.HasMeshRenderer(Selection.activeTransform))
+			{
+				EditorUtility.DisplayDialog("Error", Strings.NO_MESHRENDERER, Strings.OK);
+				return;
+			}
+
+
+			/*
+			if (!Common.IsNonPrefabGameObjectInstance(Selection.activeGameObject))
+			{
+				EditorUtility.DisplayDialog("Error", Strings.NOT_SIMPLE_GAMEOBJECT_INSTANCE, Strings.OK);
+				return;
+			}
+			*/
+
+			processGameObject(Selection.activeGameObject);
+		}
+
+		private static void processGameObject (GameObject pGameObject)
+		{
+			Transform selectedTransform = pGameObject.transform;
+			Vector3 originalPosition = selectedTransform.position;
+
+			Undo.RecordObject(selectedTransform, "Pivot point fix");
+			MeshRenderer[] meshRenderers = selectedTransform.GetComponentsInChildren<MeshRenderer>();
+
+			if (meshRenderers.Length == 0) return;
+
+			Bounds bounds = meshRenderers[0].bounds;
+			for (int i = 1; i < meshRenderers.Length; i++)
+			{
+				bounds.Encapsulate(meshRenderers[i].bounds);
+			}
+
+			List<Transform> children = new List<Transform>();
+			foreach (Transform child in selectedTransform)
+			{
+				Undo.RecordObject(child, "Pivot point fix");
+				children.Add(child);
+			}
+			
+			//move pivot point down
+			selectedTransform.position = bounds.center + Vector3.down * bounds.extents.y;
+
+			//move all objects in the transform up
+			Vector3 delta = originalPosition - selectedTransform.position;
+			foreach (Transform child in children) child.position += delta;
+		}
+	}
+}
+
